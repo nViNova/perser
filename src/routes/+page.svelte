d4e218958c3d59656699b3f12af03a9d2f03f98d
ssgonda <ssgonda@up.edu.ph>
2024-10-28 08:17:08 +0000

    obama

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let name = 'person';
+    let src = 'https://media1.tenor.com/m/t0uxZ-qpK8kAAAAd/rivesid-obama.gif';
+</script>
+
+<style>
+    div{
+        background-color: #f8f8f8;
+    }
+    h1 {
+        color: #484848;
+        font-family: sans-serif;
+    }
+</style>
+
+<div>
+    <h1>Hello {name}!</h1>
+    <img {src} alt="obama."/>
+</div>
+
