9390d3fe7c28eca5fbc5e14c9cfde5eff8469d21
ssgonda <ssgonda@up.edu.ph>
2024-10-29 17:58:11 +0000

    config fix

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["admin/config.yml","admin/index.html","favicon.png","favicon.svg","titlelogo.png","web-app-manifest-512x512.png"]),
-	mimeTypes: {".yml":"text/yaml",".html":"text/html",".png":"image/png",".svg":"image/svg+xml"},
+	assets: new Set(["admin/config.yml","admin/index.html","favicon.png","favicon.svg","media/uploads/94_big.jpg","titlelogo.png","web-app-manifest-512x512.png"]),
+	mimeTypes: {".yml":"text/yaml",".html":"text/html",".png":"image/png",".svg":"image/svg+xml",".jpg":"image/jpeg"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.iTgMctTT.js","app":"_app/immutable/entry/app.pOLJ88CS.js","imports":["_app/immutable/entry/start.iTgMctTT.js","_app/immutable/chunks/entry.D5unGtJC.js","_app/immutable/chunks/runtime.C3sxxL-a.js","_app/immutable/entry/app.pOLJ88CS.js","_app/immutable/chunks/runtime.C3sxxL-a.js","_app/immutable/chunks/store.L2tZSFGn.js","_app/immutable/chunks/disclose-version.LdpTF4bn.js","_app/immutable/chunks/proxy.C39g1Awl.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.CvnrQvkm.js","app":"_app/immutable/entry/app.VTA6hURr.js","imports":["_app/immutable/entry/start.CvnrQvkm.js","_app/immutable/chunks/entry.kBG5NF25.js","_app/immutable/chunks/runtime.C3sxxL-a.js","_app/immutable/entry/app.VTA6hURr.js","_app/immutable/chunks/runtime.C3sxxL-a.js","_app/immutable/chunks/store.L2tZSFGn.js","_app/immutable/chunks/disclose-version.LdpTF4bn.js","_app/immutable/chunks/proxy.C39g1Awl.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
