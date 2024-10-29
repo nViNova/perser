8d19f79522d1b7078e48c76ed2455e015a32256f
ssgonda <ssgonda@up.edu.ph>
2024-10-29 21:00:29 +0000

    pageserver fix

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -541,7 +541,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1els85a"
+  version_hash: "g2jgls"
 };
 async function get_hooks() {
   return {};
