b15cc87527376b2267f8a12f33485be794578e76
ssgonda <ssgonda@up.edu.ph>
2024-11-22 16:21:24 +0000

    init homepage navbar items, json fetch for individual post type draft

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,15 @@
-import { error } from '@sveltejs/kit';
+// export const load = async ({ url, fetch }) => {
+// 	const postRes = await fetch(`${url.origin}/api/posts.json`);
+// 	const posts = await postRes.json();
 
-export const load = async () => {
-	return {};
-};
+// 	const featureRes = await fetch(`${url.origin}/api/features.json`);
+// 	const features = await featureRes.json();
+
+// 	const newsRes = await fetch(`${url.origin}/api/news.json`);
+// 	const news = await newsRes.json();
+
+// 	const totalRes = await fetch(`${url.origin}/api/posts/count`);
+// 	const total = await totalRes.json();
+
+// 	return { posts, total, features, news };
+// };
