d6d569104c1a5d769e8cf938e7b81c7dcea39310
ervnmrcdo <ervinmercado31@gmail.com>
2025-04-08 09:09:06 +0800

    Add: Tab Section to Home

---
+++ src/lib/components/HomeTabs.svelte
@@ -0,0 +1,43 @@
+<script>
+	import HomeNews from "./HomeNews.svelte";
+    import HomeTabElements from "./HomeTabElements.svelte";
+
+
+	let activeItem = $state('Featured');
+	let items = ['Featured', 'Top', 'New']
+	let { data } = $props();
+
+	const tabChange = (e) => {
+		activeItem = e.detail
+	}
+
+</script>
+
+
+<div class="home-body">
+	
+	<HomeTabElements {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Featured'}
+		<div class="tab-elements">
+			<!-- <HomeNews posts={data.posts}/> -->
+			<p>wo</p>
+		</div>
+	{:else if activeItem === 'Top'}
+		<div class="tab-elements">
+			<p>ai</p>
+		</div>
+	{:else if activeItem === 'New'}
+		<div class="tab-elements">
+			<p>ni</p>
+		</div>
+	{/if}
+</div>
+
+
+<style>
+.tab-elements{
+
+	display: flex;
+	justify-content: center;
+}
+</style>
