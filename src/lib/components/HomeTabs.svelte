c61f612f554dfb7d5c7b833a813195303634c7fe
ervnmrcdo <ervinmercado31@gmail.com>
2025-05-03 11:15:46 +0800

    Add: Articles in home page

--- src/lib/components/HomeTabs.svelte
+++ src/lib/components/HomeTabs.svelte
@@ -1,29 +1,31 @@
 <script>
+	import FeaturedArticles from './FeaturedArticles.svelte';
 	import HomeTabElements from './HomeTabElements.svelte';
+	import DCSArticles from './DCSArticles.svelte';
 
-	let activeItem = $state('Featured');
-	let items = ['Featured', 'Top', 'New'];
+	let activeItem = $state('DCS');
+	let items = ['Latest News', 'Featured', 'DCS'];
+
+	let { data } = $props();
 
 	const tabChange = (e) => {
 		activeItem = e.detail;
 	};
-
-	let { posts = [] } = $props();
 </script>
 
 <div class="home-tab-component">
 	<HomeTabElements {activeItem} {items} on:tabChange={tabChange} />
-	{#if activeItem === 'Featured'}
-		<div class="tab-elements">
-			<p>wo</p>
-		</div>
-	{:else if activeItem === 'Top'}
+	{#if activeItem === 'Latest News'}
 		<div class="tab-elements">
 			<p>ai</p>
 		</div>
-	{:else if activeItem === 'New'}
+	{:else if activeItem === 'Featured'}
 		<div class="tab-elements">
-			<p>ni</p>
+			<FeaturedArticles posts={data.posts} />
+		</div>
+	{:else if activeItem === 'DCS'}
+		<div class="tab-elements">
+			<DCSArticles posts={data.posts} />
 		</div>
 	{/if}
 </div>
