791b1aefd902c8659ed6ccd448306531c46a2662
nViNova <stephenjamesgonda@gmail.com>
2025-05-09 18:59:05 +0800

    feat: update about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -17,9 +17,51 @@
 
 Take apart, Take a part.
 <br>
+
 The official site for the The UP Parser, the student-run publication organization for the Department of Computer Science at UP Diliman
 <br>
-Proudly made with SvelteKit.
+
+<h2>Who We Are</h2>
+
+<p>
+	In 1999, Jonel Uy had an idea of starting a publication for the Department with the primary purpose of information dissemination. During that time, the Engineering Logscript was defunct, and so CS students didn’t have access to important and relevant news about the college. Jonel took the initiative and sought for the approval of the department chair. He then formed a staff, with Maru Velas as the Editor-in-Chief. Parser released its first issue ever in 1999, and staked its claim as the College’s first ever department-based student publication.
+
+Unfortunately, Parser became defunct in 2002 due to financial reasons. Two years later, through the efforts of the CS Representatives Carlos Lasa, Ponch Mante, Chicco Reyes, and Michael Sepulveda, and under the leadership of Philip Kimpo Jr., The UP Parser was revived. Philip served as EIC for two years, during which the publication produced a total of five outstanding issues. Even more importantly, they expanded the circulation through a website, e-mail newsletters, and bulletin board versions.
+</p>
+
+<p>
+	Joebet del Rosario continued this legacy as EIC from AY 2006 to 2007. However, Parser became defunct yet again after that, and at AY 2008-2009, Parser underwent a revival once more withstanding 6 years then became defunct again.
+
+With the initiative of the CamINg Computer Science Representatives, The UP Parser was set to handle the documentation once again of all Department-related events in the College.
+
+For the past years, Parser has been the voice of the UP DCS student population. Staying true to its motto, “Analyzing Life the CS Way,” Parser has provided an avenue of communication and a means of self-expression for the DCS community. Moreover, Parser has proved that CS students can have writing and artistic talents, and not just programming skills.
+
+Today, in a world where print media is becoming more and more obsolete, the challenge for Parser lies in proving otherwise, especially in such a tech-savvy community like that of DCS. It has adapted to the new, emerging form of media—social media such as Facebook, Twitter, and the like—but continues to produce print copies in keeping with its rich heritage. Even more importantly, Parser believes that the Department is best served through the most accessible and most tangible medium available.
+
+Parser has always been dedicated to giving the DCS community the best, most dependable, and most passionately-made publication it can every issue. It will certainly continue to do so for this year and the years to come.
+
+</p>
+
+<h2 style="text-align:center;">The Logo</h2>
+
+<img src="/media/assets/logoSVG.svg" alt="logo">
+
+<h3>Yellow</h3>
+Some people call this shade of yellow “bumblebee.” We picked this (GenZ-ish) shade to make Parser look more eye-catching and attention-worthy.
+<h3>Hidden Ps</h3>
+If you look more closely, you can find hidden Ps! P for Parser of course!!
+<h3>Tree</h3>
+Inspired by the current logo, we wanted the new logo to also look like a parsing tree, which seeks to analyze the smaller details to see the bigger picture for the production of unambiguous, clear, and factual journalism.
+You can also observe that in the new logo, there is no defined root. This means that any node can be the root; and in the same way, any Parser member can be given the chance to take the lead.
+<h3>Node</h3>
+Again, inspired by the current logo, may nodes din ang new logo. But, there’s more to that! You can see that each node looks like the tip of a pencil (representing our writers), or camera lenses (representing our photographers and videographers). Perhaps four nodes for writing, digital art, photography, and videography.
+<h3>Close Together, but not huddling</h3>
+As you can see, prominent na may connection between the nodes. In ate Gabbi’s words,
+<blockquote>“Writers tayo, artists tayo, videographers and photographers tayo. pero ang essence naman talaga ng Parser ay it INVOLVES--affiliated or unaffiliated.”</blockquote>
+More importantly, it can be noticed that the nodes are close together, but not huddling! You can also see that there’s extra space below. This implies that another person can always enter the circle. There’s always room for one more. There’s always room for another helping hand!
+<h3>Eye</h3>
+Each node looks like an eye! In the end naman, with all the content released and that will be released, there has always been one common goal in mind: to open the eyes of each member of the DCS community.
+
 
 <h2>Attributions</h2>
 
