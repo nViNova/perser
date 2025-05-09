bb9b5f99ce896ec54cc9a7c81c7ac3e34fd8679e
nViNova <stephenjamesgonda@gmail.com>
2025-05-09 18:48:21 +0800

    feat: working form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -18,38 +18,7 @@ Feel free to reach us at our <a href="https://www.facebook.com/upparser/">Facebo
 
 <!-- <Callout>This form does nothing! Website in development.</Callout> -->
 
-<form method="POST" action="/api/submit">
-	<div class="form-section">
-		<label for="name">Name</label>
-		<input type="text" id="name" placeholder="First name" />
-	</div>
-	
-	<div class="form-section">
-		<label for="email">Email</label>
-		<input type="email" id="email" placeholder="Email address" />
-	</div>
-
-	<div class="form-section">
-		<label for="subject">
-			Subject
-		</label>
-		<input type="subject" id="subject" placeholder="Subject Here" />
-	</div>
-
-	<div class="form-section">
-		<label for="message">
-			Message Body
-		</label>
-
-		<textarea type="message" rows=10 id="message"></textarea>
-	</div>
-
-	<div class="form-section">
-		<input type="checkbox" id="c1" >
-		<label for="c1">Receive daily announcements from us!</label>
-	</div>
-
-	<input type="submit" value="Submit">
-</form>
+<iframe data-tally-src="https://tally.so/embed/mZgZrV?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="552" frameborder="0" marginheight="0" marginwidth="0" title="null"></iframe>
+<script>var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}</script>
 
 </main>
